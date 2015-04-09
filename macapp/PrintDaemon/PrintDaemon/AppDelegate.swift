dd3cb5ecc97376e1c021dc775de2cad56a026738
Charimon <acharkin@gmail.com>
2015-04-09 00:51:05 -0700

    added priter server + printer

---
+++ macapp/PrintDaemon/PrintDaemon/AppDelegate.swift
@@ -0,0 +1,86 @@
+//
+//  AppDelegate.swift
+//  PrintDaemon
+//
+//  Created by Andrew Charkin on 4/9/15.
+//  Copyright (c) 2015 Charimon. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+  @IBOutlet weak var statusMenu: NSMenu!
+
+  let statusItem = NSStatusBar.systemStatusBar().statusItemWithLength(-1)
+  let port = 8080
+  let printerName = "Matte_Daemon"
+  let ppdFile = "/usr/share/cups/model/PostscriptColor.ppd"
+  
+  func applicationDidFinishLaunching(aNotification: NSNotification) {
+    let icon = NSImage(named: "star")
+    icon?.setTemplate(true)
+    self.statusItem.image = icon
+    self.statusItem.menu = statusMenu
+    
+    _addPrinter()
+    startServer()
+  }
+
+  @IBAction func addPrinter(sender: NSMenuItem) { _addPrinter() }
+  
+  private func _addPrinter(){
+    let task = NSTask()
+    task.launchPath = "/usr/sbin/lpadmin"
+    
+    task.arguments = ["-p", printerName, "-E", "-v", "http://localhost:\(self.port)/ipp", "-P", ppdFile]
+    task.launch()
+    task.waitUntilExit()
+  }
+  
+  let responseObj = [
+    "request-id":"50",
+    "version-number":"2.0",
+    "status-code":"successful-ok",
+    "referenced-uri-scheme-supported":"file, http, https, ipp",
+    "job-uri":"/ipp/print",
+    "job-id":"1",
+    "job-state":"9",
+    "job-state-reasons":"9",
+  ]
+  
+  func startServer(){
+    let server = HttpServer()
+    server["/ipp"] = { request in
+      
+      println("body \(request.headers)")
+      println("body \(request.body)")
+      
+      let body = request.body
+      
+      if let str = body {
+        //        let newString = str.substringToIndex(advance(str.startIndex, count(str)))
+        println("HERE: \(str)")
+      }
+      
+      switch request.method.uppercaseString {
+      case "POST":
+        let data = "".dataUsingEncoding(NSUTF8StringEncoding, allowLossyConversion: true)
+        //          return HttpResponse.RAW(100, data!)
+        
+        return .OK(.JSON(self.responseObj))
+      case "GET":
+        return .OK(.JSON(self.responseObj))
+      default:
+        return .NotFound
+      }
+      
+      
+      
+    }
+    server.start(listenPort: 8080)
+  }
+
+}
+
---
+++ macapp/PrintDaemon/PrintDaemon/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  PrintDaemon
+//
+//  Created by Andrew Charkin on 4/9/15.
+//  Copyright (c) 2015 Charimon. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: AnyObject? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
